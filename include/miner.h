4b61897b75ad8ea7e4a4498798de00843de3d583
dday87 <dday87@uvic.ca>
2019-08-07 08:31:03 -0700

    Working on miner

--- include/miner.h
+++ include/miner.h
@@ -1,11 +1,31 @@
 #ifndef __MINER_H__
 #define __MINER_H__
 
+#define ENABLE_CUDA 1
+#define ENABLE OMP 2
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct miner ghostminer;
+typedef struct miner miner;
+struct miner
+{
+	char type[16] = "Prototype_Miner";
+	char ipv4address[8] = "0.0.0.0";
+	unsigned int send_port = 0;
+	unsigned int listen_port = 0;
+	char socktype[4] = "tcp";
+};
+
+typedef struct ghostminer ghostminer;
+struct ghostminer
+{
+	char type[16] = "MPI_CUDA_Miner";
+	char ipv4address[8] = "0.0.0.0";
+	unsigned int port = 0;
+	char socktype[4] = "tcp";
+};
 
 #ifdef __cplusplus
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include <math.h>
 #include "../include/hash.h"
 #include "../include/miner.h"
-#include "../include/mainwindow.h"
 
 int main(int argc, char *arg[])
 {
